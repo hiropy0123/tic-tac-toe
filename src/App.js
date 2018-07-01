9ac5ce5cef22d895fea80b5b7b9f34d9b3d30c87
HirokiWindowsPC <hiroki123naka4ma@gmail.com>
2018-07-01 22:25:30 +0900

    Passing Data Through Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
-  name = "Yamada";
+class Square extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hello, {this.name}!</h1>
-        </header>
-      </div>
-    );
+      <button className="square">
+        {this.props.value}
+      </button>
+    )
   }
 }
 
-export default App;
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i}/>
+  }
+
+  render() {
+    const status = "Next Player: X"
+
+    return(
+      <React.Fragment>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
