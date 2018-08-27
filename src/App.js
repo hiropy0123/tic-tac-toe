5fa915906e322ba679f8ef606fec99c69f59a1c2
HirokiWindowsPC <hiroki123naka4ma@gmail.com>
2018-08-27 23:17:35 +0900

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 import './App.css'
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'}) }>
+        {this.state.value}
       </button>
     )
   }
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
     return <Square value={i}/>
   }
