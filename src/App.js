7ca02521a87bf36d654975b28915311e75b2b3a5
HirokiWindowsPC <hiroki123naka4ma@gmail.com>
2018-08-28 18:40:03 +0900

    next> Functional Components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react'
 import './App.css'
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'}) }>
-        {this.state.value}
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick() }
+      >
+        {this.props.value}
       </button>
     )
   }
@@ -26,8 +22,19 @@ class Board extends Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+       />
+    )
   }
 
   render() {
